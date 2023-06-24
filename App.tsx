d7a34807687da008ae7c8c683b3619e0b9cb6a94
Zohaib-Ramzan <zohaibramzan711@gmail.com>
2023-06-24 20:25:19 +0500

    Final Completion with Shapes

--- App.tsx
+++ App.tsx
@@ -1,118 +1,155 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  StatusBar,
+  TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [bgChanger, setbgChanger] = useState('#000000');
+  const [rectBgChanger, setrectBgChanger] = useState('');
+  const [squareBgChanger, setsquareBgChanger] = useState('');
+  const [circleBgChanger, setcircleBgChanger] = useState('');
+  const [triangleBgChanger, settriangleBgChanger] = useState('');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  function colorChanger() {
+    const hexRange = '0123456789ABCDEF';
+    let bgcolor = '#';
+    let rectbgcolor = '#';
+    let squarebgcolor = '#';
+    let circlebgcolor = '#';
+    let trianglebgcolor = '#';
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    for (let i = 0; i < 6; i++) {
+      bgcolor += hexRange[Math.floor(Math.random() * 16)];
+      rectbgcolor += hexRange[Math.floor(Math.random() * 16)];
+      squarebgcolor += hexRange[Math.floor(Math.random() * 16)];
+      circlebgcolor += hexRange[Math.floor(Math.random() * 16)];
+      trianglebgcolor += hexRange[Math.floor(Math.random() * 16)]
+    }
+    console.log(bgcolor);
+    setbgChanger(bgcolor);
+    setrectBgChanger(rectbgcolor);
+    setsquareBgChanger(squarebgcolor);
+    setcircleBgChanger(circlebgcolor);
+    settriangleBgChanger(trianglebgcolor);
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <StatusBar backgroundColor={bgChanger} />
+      <View style={[styles.container, {backgroundColor: bgChanger}]}>
+        <View style={styles.shapesContainer}>
+          <View style={[styles.rectView, {backgroundColor: rectBgChanger}]}>
+            <Text style={styles.rectTxt}>Rectangle</Text>
+          </View>
+          <View style={[styles.squareView, {backgroundColor: squareBgChanger}]}>
+            <Text style={styles.squareTxt}>Square</Text>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <TouchableOpacity onPress={colorChanger}>
+          <Text style={styles.btnTxt}>Press Me</Text>
+        </TouchableOpacity>
+        <View style={styles.shapesContainer2}>
+          <View style={[styles.circleView , {backgroundColor : circleBgChanger}]}>
+            <Text style={styles.circleTxt}>Circle</Text>
+          </View>
+          <View style={[styles.triangleView , {borderBottomColor : triangleBgChanger}]}>
+            <Text style={styles.triangleTxt}>Triangle</Text>
+          </View>
+        </View>
+      </View>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnTxt: {
+    backgroundColor: '#758283',
+    padding: 5,
+    paddingLeft: 20,
+    height: 30,
+    width: 100,
+    textTransform: 'uppercase',
+    borderRadius: 5,
+  },
+  shapesContainer: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 60,
+  },
+  rectView: {
+    position: 'absolute',
+    width: 180, // Adjust the width as desired
+    height: 100, // Adjust the height as desired
+    backgroundColor: '#FF0000', // Adjust the background color as desired
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+  rectTxt: {
+    color: '#fff',
+    justifyContent: 'center',
+  },
+  squareView: {
+    position: 'absolute',
+    top: 100,
+    height: 100,
+    width: 100,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  squareTxt: {
+    color: '#FFF',
+  },
+  shapesContainer2: {
+    position: 'absolute',
+    bottom: 150,
+    left : 40,
+    flexDirection : "row",
+  },
+  circleView: {
+  height : 100,
+  width : 100,
+  borderRadius : 50,
+  backgroundColor : "coral",
+  justifyContent : "center",
+  alignItems : "center",
+  },
+  circleTxt: {
+    color : "#FFF"
+  },
+  triangleView : {
+marginHorizontal : 80,
+width: 0,
+height: 0,
+backgroundColor: 'transparent',
+borderStyle: 'solid',
+borderTopWidth: 0,
+borderRightWidth: 45,
+borderBottomWidth: 90,
+borderLeftWidth: 45,
+borderTopColor: 'transparent',
+borderRightColor: 'transparent',
+borderBottomColor: 'red',
+borderLeftColor: 'transparent',
+justifyContent : 'flex-end',
+alignItems:"center",
+  } , 
+  triangleTxt : {
+color : "#FFF",
+marginBottom : 10,
+position: 'absolute',
+  }
+});
